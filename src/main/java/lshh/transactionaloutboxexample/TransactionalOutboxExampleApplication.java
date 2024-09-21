bf45d5a10d7189f781e61108c567ba95275a3969
lshh <markhong93@naver.com>
2024-09-21 16:14:48 +0900

    Initialize Spring Boot project with Gradle
    Add essential Gradle build files, Spring Boot configuration, and infrastructure setup for Kafka. This includes Docker compose files and example command scripts for both Kafka and Redis.

---
+++ src/main/java/lshh/transactionaloutboxexample/TransactionalOutboxExampleApplication.java
@@ -0,0 +1,13 @@
+package lshh.transactionaloutboxexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalOutboxExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalOutboxExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lshh/transactionaloutboxexample/TransactionalOutboxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package lshh.transactionaloutboxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalOutboxExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
